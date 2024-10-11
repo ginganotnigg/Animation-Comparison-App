07cbb7663d3a1f39ab16ee41eb87f2c66be46b9d
duckdzung <laiducdung1911@gmail.com>
2024-10-11 13:22:57 +0700

    fix: re-design button press screen

--- lib/screen/button_press.dart
+++ lib/screen/button_press.dart
@@ -1,20 +1,48 @@
+import 'package:animation_app/model/movie.dart';
 import 'package:flutter/material.dart';
-import 'package:rive/rive.dart';
+import 'package:rive/rive.dart' as rive;
 
 class ButtonPressScreen extends StatefulWidget {
   final bool useBuiltIn;
 
-  const ButtonPressScreen({super.key, required this.useBuiltIn});
+  const ButtonPressScreen({Key? key, required this.useBuiltIn})
+      : super(key: key);
 
   @override
-  State<ButtonPressScreen> createState() => _ButtonPressScreenState();
+  _ButtonPressScreenState createState() => _ButtonPressScreenState();
 }
 
 class _ButtonPressScreenState extends State<ButtonPressScreen>
     with SingleTickerProviderStateMixin {
-  SMITrigger? _onClick;
+  final TextEditingController _searchController = TextEditingController();
+  List<Movie> _searchResults = [];
+  rive.SMITrigger? _onClick;
   double _scale = 1.0;
   double _shadowBlur = 0.0;
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  void _searchMovies() async {
+    setState(() {
+      _searchResults = [];
+    });
+
+    String query = _searchController.text.toLowerCase();
+
+    List<Movie> results = sampleMovies()
+        .where((movie) => movie.title.toLowerCase().contains(query))
+        .toList();
+
+    setState(() {
+      _searchResults = results;
+    });
+
+    _searchController.text = '';
+  }
 
   void _onButtonPressed() {
     if (_onClick != null) {
@@ -23,7 +51,7 @@ class _ButtonPressScreenState extends State<ButtonPressScreen>
 
     setState(() {
       _scale = 0.8;
-      _shadowBlur = 70.0;
+      _shadowBlur = 40.0; // Blur shadow effect
     });
 
     Future.delayed(const Duration(milliseconds: 100), () {
@@ -31,82 +59,136 @@ class _ButtonPressScreenState extends State<ButtonPressScreen>
         _scale = 1.0;
         _shadowBlur = 0.0; // Reset the shadow after the effect
       });
+      _searchMovies(); // Execute search when button is pressed
     });
   }
 
-  // Initialize Rive state machine
-  void _onRiveInit(Artboard artboard) {
-    final controller = StateMachineController.fromArtboard(
-        artboard, 'State Machine 1'); // State machine name
+  void _onRiveInit(rive.Artboard artboard) {
+    final controller =
+        rive.StateMachineController.fromArtboard(artboard, 'State Machine 1');
     if (controller != null) {
       artboard.addController(controller);
-      _onClick = controller.findInput('CLICK') as SMITrigger?;
+      _onClick = controller.findInput('CLICK') as rive.SMITrigger?;
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: widget.useBuiltIn
-            // Flutter Button
-            ? AnimatedScale(
-                scale: _scale,
-                duration: const Duration(milliseconds: 100),
-                curve: Curves.easeInOut,
-                child: AnimatedContainer(
-                  duration: const Duration(milliseconds: 200),
-                  decoration: BoxDecoration(
-                    color: const Color(0xFF2895FF),
-                    borderRadius: BorderRadius.circular(50),
-                    border: Border.all(
-                      color: const Color(0xFF8EC6FF),
-                      width: 4,
-                    ),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.blue.withOpacity(0.2),
-                        blurRadius: _shadowBlur,
-                        spreadRadius: _shadowBlur,
-                        offset: const Offset(0, 0),
-                      ),
-                    ],
-                  ),
-                  child: ElevatedButton(
-                    onPressed: _onButtonPressed,
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.transparent,
-                      shadowColor: Colors.transparent,
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 48, vertical: 24),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                    ),
-                    child: const Text(
-                      'Flutter Button',
-                      style: TextStyle(
-                        fontSize: 33,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ),
-              )
-            :
-            // Rive Button
-            GestureDetector(
-                onTap: _onButtonPressed,
-                child: SizedBox(
-                  width: 400,
-                  height: 400,
-                  child: RiveAnimation.asset(
-                    'lib/assets/rive/button_press.riv',
-                    fit: BoxFit.cover,
-                    onInit: _onRiveInit, // Initialize the Rive animation
-                  ),
-                ),
+      appBar: AppBar(title: const Text('Button Press Comparison')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            // TextField for movie title
+            TextField(
+              controller: _searchController,
+              decoration: const InputDecoration(
+                labelText: 'Movie Name',
+                border: OutlineInputBorder(),
               ),
+            ),
+            const SizedBox(height: 16),
+            // Search button: Built-in or Rive
+            widget.useBuiltIn ? _buildBuiltInButton() : _buildRiveButton(),
+            const SizedBox(height: 16),
+            // List of search results
+            Expanded(
+              child: _searchResults.isNotEmpty
+                  ? ListView.builder(
+                      itemCount: _searchResults.length,
+                      itemBuilder: (context, index) {
+                        final movie = _searchResults[index];
+                        return ListTile(
+                          contentPadding: const EdgeInsets.all(8.0),
+                          leading: FadeInImage.assetNetwork(
+                            placeholder: 'lib/assets/images/default.png',
+                            image: movie.imageUrl,
+                            width: 50,
+                            fit: BoxFit.cover,
+                            imageErrorBuilder: (context, error, stackTrace) {
+                              return const Icon(
+                                Icons.broken_image,
+                                size: 50,
+                              );
+                            },
+                          ),
+                          title: Text(movie.title),
+                          subtitle: Text('Release: ${movie.releaseDate}'),
+                        );
+                      },
+                    )
+                  : const Center(
+                      child: Text(
+                        'No movies found.',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBuiltInButton() {
+    return AnimatedScale(
+      scale: _scale,
+      duration: const Duration(milliseconds: 100),
+      curve: Curves.easeInOut,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 200),
+        decoration: BoxDecoration(
+          color: const Color(0xFF2895FF),
+          borderRadius: BorderRadius.circular(50),
+          border: Border.all(
+            color: const Color(0xFF8EC6FF),
+            width: 4,
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.blue.withOpacity(0.2),
+              blurRadius: _shadowBlur,
+              spreadRadius: _shadowBlur,
+              offset: const Offset(0, 0),
+            ),
+          ],
+        ),
+        child: ElevatedButton(
+          onPressed: _onButtonPressed,
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.transparent,
+            shadowColor: Colors.transparent,
+            padding: const EdgeInsets.symmetric(horizontal: 64, vertical: 15),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+          ),
+          child: const Text(
+            'Search',
+            style: TextStyle(fontSize: 28, color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildRiveButton() {
+    return GestureDetector(
+      onTap: _onButtonPressed,
+      child: AnimatedScale(
+        scale: _scale,
+        duration: const Duration(milliseconds: 100),
+        curve: Curves.easeInOut,
+        child: SizedBox(
+          width: 240, // Adjust size as needed
+          height: 80, // Adjust size as needed
+          child: rive.RiveAnimation.asset(
+            'lib/assets/rive/button_press.riv',
+            onInit: _onRiveInit,
+            // fit: BoxFit.contain,
+          ),
+        ),
       ),
     );
   }
